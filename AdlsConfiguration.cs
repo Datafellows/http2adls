a81b127de70b707286d2a57d33d2133809535e94
Robert Hartskeerl <robert.hartskeerl@datafellows.nl>
2020-09-22 14:27:23 +0200

    Added Filename

--- AdlsConfiguration.cs
+++ AdlsConfiguration.cs
@@ -1,10 +1,28 @@
+using System;
+
 namespace DataFellows.HttpConsumer
 {
     public class AdlsConfiguration
     {
+        private string _filename;
         public string ConnectionString { get; set; }
 
         public string Container { get; set; }
-        public string Filename { get; set; }
+        public string Filename 
+        { 
+            get
+            {
+                return _filename
+                    .Replace("{year}", DateTime.UtcNow.Year.ToString("0000"))
+                    .Replace("{month}", DateTime.UtcNow.Month.ToString("00"))
+                    .Replace("{day}", DateTime.UtcNow.Day.ToString("00"))
+                    .Replace("{hour}", DateTime.UtcNow.Hour.ToString("00"))
+                    .Replace("{minute}", DateTime.UtcNow.ToString("00"));
+            }
+            set
+            {
+                _filename = value;
+            }
+        }
     }
 }
